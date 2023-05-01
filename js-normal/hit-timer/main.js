f727166e2404c164e94e54e9c2e8fd4c9276c89c
shimomura95 <lt1273fr@ed.ritsumei.ac.jp>
2023-05-01 10:34:04 +0900

    hit-timer commit

--- js-normal/hit-timer/main.js
+++ js-normal/hit-timer/main.js
@@ -1,10 +1,22 @@
-const startTimer = document.getElementById('startTimer');
+// 初見
+/*const startTimer = document.getElementById('startTimer');
 const confirmTime = document.getElementById('confirmTime');
 
 var timer;
 
-function timePass() {
+var pass;
+
+function startTime() {
+    timer = 0;
     timer++;
+    document.getElementById('start').disabled = true;
+    setTimeout(function () {
+        clearInterval(timer);
+    }, 40000)
+}
+
+function confirmTimer() {
+    clearInterval(timer);
     word1 = `${timer}経過、ずれている。`;
     word2 = `${timer}経過、ピッタリ。`;
     if (timer === 20) {
@@ -14,15 +26,81 @@ function timePass() {
     }
 }
 
-var pass;
+startTimer.addEventListener('click', function () {
+    timer = setInterval(startTime(), 100);
+})
 
-function startTimer() {
-    timer = 0;
-    pass = setInterval(timePass(), 100);
-    document.getElementById('start').disabled = true;
+confirmTime.addEventListener('click', function () {
+    confirmTimer();
+})*/
 
-}
+//完成
+/*let timer;
+let sec = 0;
 
-function confirmTime() {
+const startTimer = document.getElementById('startTimer');
+const confirmTime = document.getElementById('confirmTime');
 
+confirmTime.addEventListener('click', function () {
+    if (sec === 20) {
+        alert('成功');
+    } else if (sec < 20) {
+        alert(`現在${sec}秒、失敗：まだ早い!`);
+    } else {
+        alert(`現在${sec}秒、再挑戦!!`);
+    }
+    clearInterval(timer);
+    sec = 0;
+})
+
+startTimer.addEventListener('click', function () {
+    clearInterval(timer);
+    timer = setInterval('count()', 1000)
+})
+
+const count = function () {
+    sec += 1;
+    if (sec === 40) {
+        clearInterval(timer);
+        alert('終了');
+        sec = 0;
+    }
+}*/
+
+//チャレンジ
+let timer;
+
+const confirmTime2 = document.getElementById('confirmTime2');
+const startTimer2 = document.getElementById('startTimer2');
+
+let startTime;
+let nowTime;
+let stopTime;
+
+confirmTime2.addEventListener('click', function () {
+    nowTime = new Date();
+    if (startTime === undefined) {
+        return;
+    }
+    if (stopTime === 20) {
+        alert('成功');
+    } else if (stopTime < 20) {
+        alert(`現在${stopTime}、失敗`)
+    } else {
+        alert(`現在${stopTime}、再挑戦`)
+    }
+    clearInterval(timer);
+})
+
+startTimer2.addEventListener('click', function () {
+    startTime = new Date();
+    timer = setInterval('count2()', 1000);
+})
+
+const count2 = function () {
+    let checkTime = new Date();
+    stopTime = Math.floor((checkTime.getTime() - startTime.getTime()) / 1000);
+    if (stopTime === 40) {
+        alert('終了');
+    }
 }
